dee18a2a7034c0e0c81002bb051c19c467bdb3ac
AIe !! <makramjandar@gmail.com>
2019-12-26 00:39:00 +0100

    ✔️

--- Publications/Medium/MSSQL/One-Shot-Upswing/5.WithLead.sql
+++
@@ -1,6 +0,0 @@
------------------------------------------------------------------------------- With Analytic Function
-SELECT *
-	, LEAD(Score) OVER (ORDER BY Id) Lead
-FROM GradesDetail
-WHERE Test = 'D';
-GO
